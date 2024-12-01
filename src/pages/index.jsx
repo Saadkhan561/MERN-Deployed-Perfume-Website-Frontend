5cd409c455dadb0c8be36e54410dfb36d1084d05
Saad Khan <saadnadeem885@gmail.com>
2024-12-01 04:37:52 -0800

    ad div fixed

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -38,14 +38,14 @@ export default function Home() {
         {/* MAIN AD DIV */}
         <div className="flex justify-center h-max mob_display:pt-0">
           <Slider
-            className="w-full sm:h-[300px] md:h-[400px] h-[250px] z-20"
+            className="w-full sm:h-[300px] md:h-[400px] h-[180px] z-20"
             {...settings}
           >
             {/* {Array.from({length: 3}).map((img, index) => (
               <SliderImageCard key={index} imgName={index+1} />
             ))} */}
             <Image
-              className="sm:h-[300px] md:h-[400px] h-[250px] w-full"
+              className="sm:h-[300px] md:h-[400px] h-[180px] w-full"
               src="/images/ad_1.jpg"
               alt=""
               width={1600}
@@ -53,7 +53,7 @@ export default function Home() {
               style={{ width: "100%", height: "100%", objectFit: "cover" }}
             />
             <Image
-              className="sm:h-[300px] md:h-[400px] h-[250px] w-full"
+              className="sm:h-[300px] md:h-[400px] h-[180px] w-full"
               src="/images/ad_2.jpg"
               alt=""
               width={1600}
@@ -61,7 +61,7 @@ export default function Home() {
               style={{ width: "100%", height: "100%", objectFit: "cover" }}
             />
             <Image
-              className="sm:h-[300px] md:h-[400px] h-[250px] w-full"
+              className="sm:h-[300px] md:h-[400px] h-[180px] w-full"
               src="/images/ad_3.jpg"
               alt=""
               width={1600}
